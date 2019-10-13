0c565d4c896d5e6753b59e0339b78ed31788208f
Pranjo Sudjman <dicroatiano@gmail.com>
2019-10-13 18:13:33 +0200

    Added HelloApi get

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+
+    def get(self, request, format=None):
+        an_apiview = [
+            'nesto nesto',
+            'isto tako nesto',
+            'jos i ovo'
+        ]
+        return Response({'message': 'jeste', 'an_apiview': an_apiview})

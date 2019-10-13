2551e50cabed812c72d6aa21f2e040b2154f97a6
Pranjo Sudjman <dicroatiano@gmail.com>
2019-10-13 18:48:46 +0200

    Added HelloApi get, post, put and serializer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
+from profiles_api import serializers
 
 class HelloApiView(APIView):
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         an_apiview = [
@@ -11,3 +15,18 @@ class HelloApiView(APIView):
             'jos i ovo'
         ]
         return Response({'message': 'jeste', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({'message': 'zamjeni cijeli objekt za onaj objekt koji je u request-u'})
